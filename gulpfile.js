2af0b3c669e9640d3e25c28e2d1746348698175b
Derek Bell <luicd@lucid.com.au>
2015-07-28 18:02:34 +1000

    gulp auto prefiexer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -35,5 +36,9 @@ gulp.task('sass', function () {
   gulp.src('assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+        cascade: false
+        }))
     .pipe(gulp.dest('assets/css'));
 });
