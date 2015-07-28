1ebf450a7530470be6a8de0b554db89ad792d831
Derek Bell <luicd@lucid.com.au>
2015-07-28 17:40:58 +1000

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
 var gulp = require('gulp');
 
+var sass = require('gulp-sass');
+
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+
+/*
+CSS
+Compile sass
+add browser prefix
+minify
+
+Images
+minify images
+combine svg
+make png backup files
+make sprite sheet
+
+JS
+concat JS
+minify JS
+
+web
+Live reload
+
+*/
+
+
+/* CSS Files */
+
+gulp.task('sass', function () {
+  gulp.src('assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('assets/css'));
 });
