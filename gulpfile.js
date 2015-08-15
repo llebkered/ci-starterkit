eacdc8007663093b8775c9bfe8906e3a7c149aed
Derek Bell <luicd@lucid.com.au>
2015-08-15 17:12:20 +1000

    CSSO compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
+// Gulp & utilities
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 
+// CSS
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
 
+// Image
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+var svgfallback = require('gulp-svgfallback');
+
+// Javascript
 
 
 
@@ -32,9 +42,10 @@ Live reload
 
 */
 
-
+/* ================= */
 /* CSS Files */
 
+// Compile SASS and Autoprefix.
 gulp.task('sass', function() {
   gulp.src('assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -44,11 +55,15 @@ gulp.task('sass', function() {
     .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
       cascade: true
     }))
+    .pipe(csso())
     .pipe(gulp.dest('assets/css'));
 });
 
+
+/* ================= */
 /* Images */
 
+// Compress images
 gulp.task('images', function() {
   return gulp.src('assets/images/*')
     .pipe(imagemin({
@@ -61,6 +76,35 @@ gulp.task('images', function() {
     .pipe(gulp.dest('assets/build/images'));
 });
 
+// Combine SVGs into 1 file
+gulp.task('svgstore', function () {
+    return gulp
+        .src('assets/svgs/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(gulp.dest('assets/build/svg'));
+});
+
+// Create PNG sprite sheet fallback
+gulp.task('svgfallback', function () {
+    return gulp
+        .src('assets/svgs/*.svg', {base: 'assets/build/svg'})
+        .pipe(svgfallback())
+        .pipe(gulp.dest('assets/build/images/png'));
+});
+
+
+/* ================= */
 /* Gulp Watch */
 
 gulp.task('watch', function() {
@@ -76,4 +120,4 @@ gulp.task('watch', function() {
   // place code for your default task here
 });
 */
-gulp.task('default', ['sass', 'images', 'watch']);
+gulp.task('default', ['sass', 'images' , 'svgstore', 'svgfallback','watch']);
