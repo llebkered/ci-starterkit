fc2cb03d158059d858b4bc211315833c9e602b4c
Derek Bell <lucid@lucid.com.au>
2015-09-08 23:25:42 +1000

    MY controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller
+{
+    public $data = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        //$this->output->enable_profiler(true);
+    }
+}
+
+/* Admin Controller */
+class Admin_Controller extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->ion_auth->logged_in()) {
+            redirect('user/login');
+        }
+    }
+}
+
+/* Public Controller */
+class Public_Controller extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // if offline load offline page
+
+    }
+}
